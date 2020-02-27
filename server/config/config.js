7dcc602aad7b68a65f404148407c1535dd359e2a
JuanR <jrxxiii@gmail.com>
2020-02-27 09:30:09 -0500

    Después de actualizar heroku

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,21 @@
 // ====================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ====================
+//     ENTORNO
+// ====================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ====================
+//     BASE DE DATOS
+// ====================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://JR23:l1ipZS0ii9KycLEE@cluster0-ap1u4.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
